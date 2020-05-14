bbe6d800e2623a1f2bf0e82273ab2082925d48b4
ahmedatef00 <ahmedatef62437@gmail.com>
2020-05-14 13:14:47 +0200

    minor fix

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 module.exports = (passport) => {
     //tokens from berrar auth
     let config = {};
-    config.secretOrKey = process.env.secret || 'islam';
+    config.secretOrKey = process.env.secret;
     config.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
     //from jwtPayload we can reach the token it self as we did in controller :{ _id: user._id } this is the Payload
     passport.use(new jwtStrategy(config, async (jwtPayload, done) => { //done may ve like next but it pass the error 
