a8365b4956379d89e866280e2c4005853dc3337d
ahmedatef00 <ahmedatef62437@gmail.com>
2020-05-16 13:57:34 +0200

    fixing some issues

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const bookController = require("../controllers/book.controller");
-
+router.get("/popular", bookController.getPopularBooks);
 router.get("/", bookController.getAllBooks);
 router.get("/:id", bookController.getBookById);
-router.post("/new", bookController.create);
+router.post("/new", bookController.createBook);
 router.delete("/:id", bookController.deleteById);
 router.put("/:id", bookController.updateById);
-router.get("/populars", bookController.getPopularBooks);
-
-module.exports = router;
+module.exports=router;
